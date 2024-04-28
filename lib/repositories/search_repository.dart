bd38eee022ef6fcd0530c26d11e239021ba0c944
Nuxt Tork <nuxt.tork@gmail.com>
2024-04-28 15:52:10 +0600

    Product search by image from home page successfully done but code comment in for HR manager approval, in ProductRepository page getHomeProducts function passing dynamic version and from home page passing context

--- lib/repositories/search_repository.dart
+++ lib/repositories/search_repository.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:typed_data';
 import 'package:kirei/app_config.dart';
 import 'package:http/http.dart' as http;
 import 'package:kirei/data_model/product_mini_response_old.dart';
 
 import 'package:kirei/helpers/shared_value_helper.dart';
+import 'package:kirei/ui_elements/product_card.dart';
 
 class SearchRepository {
   Future<ProductMiniResponse> getSearchSuggestionListResponse(
@@ -18,5 +22,33 @@ class SearchRepository {
     );
     print(response.body.toString());
     return productMiniResponseFromJson(response.body);
+  }
+
+  Future<ProductMiniResponse> getSearchByImageProductListResponse(
+      {Uint8List imageBytes, String imageName}
+      ) async {
+
+    ///upload image for search product by multipart request
+    Uri url = Uri.parse(
+        "${AppConfig.BASE_URL}/gigalogy/items/search");
+      var request = http.MultipartRequest('POST', url);
+
+
+    var image = http.MultipartFile.fromBytes('image', imageBytes, filename: imageName);
+    request.files.add(image);
+
+      request.fields['order_by'] = "default";
+
+      var response = await http.Response.fromStream(await request.send());
+
+      print('${response.body}');
+
+      if (response.statusCode == 200) {
+        print('Image uploaded successfully');
+      } else {
+        print('Failed to upload image. Error: ${response.reasonPhrase}');
+      }
+    return productMiniResponseFromJson(response.body);
+    //return jsonDecode(response.body);
   }
 }
